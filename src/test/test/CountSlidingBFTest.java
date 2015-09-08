4b5241c66fc7eca90b4465a02ddfb7ff6758665a
sophiesongge <sophiesongge@gmail.com>
2015-09-08 15:31:51 +0200

    test for CSBF not yet implemented

---
+++ src/test/test/CountSlidingBFTest.java
@@ -0,0 +1,54 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CountSlidingBFTest {
+
+	@Test
+	public void testCountSlidingBFIntIntDoubleInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountSlidingBFIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCountSlidingBFIntIntDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContainsString() {
+		fail("Not yet implemented");
+	}
+
+}
