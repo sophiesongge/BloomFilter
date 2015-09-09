44c38f1a366d3c8fa03d7429801fd81424faf0ea
sophiesongge <sophiesongge@gmail.com>
2015-09-09 12:13:32 +0200

    finish the test for Count Based Sliding Window Bloom Filter

--- src/test/test/CountSlidingBFTest.java
+++ src/test/test/CountSlidingBFTest.java
@@ -2,13 +2,35 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+import java.util.UUID;
+
 import org.junit.Test;
 
-public class CountSlidingBFTest {
+import BloomFilter.BloomFilter;
+import BloomFilter.CountSlidingBF;
 
+public class CountSlidingBFTest {
+	
+	static Random r = new Random();
+	
 	@Test
 	public void testCountSlidingBFIntIntDoubleInt() {
-		fail("Not yet implemented");
+		System.out.println("A test for csbf(slidingWindowSize, numberOfGeneration, c, k)");
+		
+		for(int i=0; i<10000; i++){
+			double c = r.nextInt(20)+1;
+			int slidingWindowSize = r.nextInt(10000)+1;
+			int numberOfGeneration = r.nextInt(20)+1;
+			int k = r.nextInt(20)+1;
+			
+			CountSlidingBF csbf = new CountSlidingBF(slidingWindowSize, numberOfGeneration, c, k);
+			
+			assertEquals(csbf.BloomFilter.getK(), k);
+			assertEquals(csbf.BloomFilter.getExpectedBitsPerElement(), c, 0);
+			assertEquals(csbf.BloomFilter.size(), c*slidingWindowSize, 0);
+			
+		}
 	}
 
 	@Test
@@ -23,17 +45,35 @@ public class CountSlidingBFTest {
 
 	@Test
 	public void testAddByteArray() {
-		fail("Not yet implemented");
+		System.out.println("A test for method add()");
+		CountSlidingBF csbf = new CountSlidingBF(120, 30, 0.001);
+        for (int i = 0; i < 120; i++) {
+            String str = UUID.randomUUID().toString();
+            csbf.add(str);
+            assert(csbf.contains(str));
+        }
 	}
 
 	@Test
 	public void testAddString() {
-		fail("Not yet implemented");
+		System.out.println("A test for method add()");
+		CountSlidingBF csbf = new CountSlidingBF(120, 30, 0.001);
+        for (int i = 0; i < 120; i++) {
+            String str = UUID.randomUUID().toString();
+            csbf.add(str);
+            assert(csbf.contains(str));
+        }
 	}
 
 	@Test
 	public void testAddE() {
-		fail("Not yet implemented");
+		System.out.println("A test for method add()");
+		CountSlidingBF csbf = new CountSlidingBF(120, 30, 0.001);
+        for (int i = 0; i < 120; i++) {
+            String str = UUID.randomUUID().toString();
+            csbf.add(str);
+            assert(csbf.contains(str));
+        }
 	}
 
 	@Test
@@ -43,7 +83,14 @@ public class CountSlidingBFTest {
 
 	@Test
 	public void testContainsE() {
-		fail("Not yet implemented");
+		System.out.println("A test for method contains()");
+		CountSlidingBF csbf = new CountSlidingBF(120, 30, 0.001);
+
+        for (int i = 0; i < 120; i++) {
+            csbf.add(Integer.toBinaryString(i));
+            assert(csbf.contains(Integer.toBinaryString(i)));
+        }
+        assertFalse(csbf.contains(UUID.randomUUID().toString()));
 	}
 
 	@Test
